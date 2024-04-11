9dfa484a9dd3e076a2979c55fc92be90f9e551da
Juan Guimarães <juandasmerces@outlook.com>
2024-04-11 20:13:00 -0300

    deletando arquivos JS

--- src/scripts/main.js
+++
@@ -1,9 +0,0 @@
-document.addEventListerner('DOMContentLoaded', function(){
-
-    function fAbreGuias() {
-        var vGuias = document.getElementById('guias');
-            vGuias.addEventListener ('click', function() {
-                vGuias.toggle("hide");
-            }) 
-        } 
-})
